9574b86bff6ce7341be5511dd3fa8e78e3dc1cc9
sfK <sfk.sjm@gmail.com>
2022-09-19 21:57:41 +0900

    c

--- 1-1/3-16-2022/program1.c
+++ 1-1/3-16-2022/program1.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main(void)
-{
-        printf("\n=============실습1=============\n");
-        printf("2021년 1학기\n");
-        printf("프로그래밍실습\n");
-        printf("3학점, 이론 2시간, 실습 2시간\n");
+int main(void) {
+    printf("\n=============실습1=============\n");
+    printf("2021년 1학기\n");
+    printf("프로그래밍실습\n");
+    printf("3학점, 이론 2시간, 실습 2시간\n");
 
     return 0;
 }
