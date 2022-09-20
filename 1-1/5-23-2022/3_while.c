21f6f6daf9a449781be6a8a8cdc97f73a4b1697b
sfK <sfk.sjm@gmail.com>
2022-09-20 22:57:31 +0900

    c

--- 1-1/5-23-2022/3_while.c
+++ 1-1/5-23-2022/3_while.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int x = 1, cnt = 0;
-    while (x != 0)
-    {
+    while (x != 0) {
         printf("x = ");
         scanf("%d", &x);
         cnt++;
